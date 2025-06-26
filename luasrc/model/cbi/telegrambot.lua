3ee6ad4e05c2ad539806416627828475d5045ba7
tokektv <168657385+tokektv@users.noreply.github.com>
2025-06-26 09:46:44 +0700

    Update telegrambot.lua

--- luasrc/model/cbi/telegrambot.lua
+++ luasrc/model/cbi/telegrambot.lua
@@ -1,1 +1,11 @@
+m = Map("telegrambot", "Telegram Bot")
 
+s = m:section(NamedSection, "config", "telegrambot", "Konfigurasi")
+s.addremove = false
+s.anonymous = true
+
+s:option(Value, "bot_token", "Bot Token")
+s:option(Value, "chat_id", "Chat ID")
+s:option(Value, "router_id", "Router ID")
+
+return m
